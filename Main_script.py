de8bcbbda1d3dcea312f8a0e26236b0a2c0c4875
Marina <marina@marina-Lenovo-IdeaPad-Z500>
2016-07-13 14:36:03 +0300

    addition of the file

---
+++ Main_script.py
@@ -0,0 +1,5 @@
+import hb_trace_parsing
+list_of_interesting_residues = ['GLU51', 'ASP52', 'GLY53', 'ARG54', 'GLU24', 'LYS63']
+all_inner, all_outer = hb_trace_parsing.all_interactions("hb_trace.dat", 20)
+hb_trace_parsing.interesting_interactions(list_of_interesting_residues, all_inner, all_outer)
+
