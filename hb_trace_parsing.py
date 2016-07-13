5cce601def062432b316f1ecf769cb7d37047220
Marina <marina@marina-Lenovo-IdeaPad-Z500>
2016-07-13 14:39:56 +0300

    lists instead files, parameters

--- hb_trace_parsing.py
+++ hb_trace_parsing.py
@@ -1,7 +1,6 @@
-def all_interactions():
+def all_interactions(filename, n):
     import re
-    n = 20 #int(input())
-    with open("hb_trace.dat", "r") as data:
+    with open(filename, "r") as data:
         strings = data.readlines()
         length = (len(strings)-1)//n
         int_names = strings[0].split(" ")
@@ -42,54 +41,51 @@ def all_interactions():
             else:
                 if name not in l_out:
                     l_out.append(name)
+        all_inner = []
         for i in range(48):#Number_of_molecules
-            with open(str(i)+"_all_inner.txt", "w") as f:
-                for element in l_inn:
-                    items = element.split("_")
-                    inn_int = items[0]+"~"+str(i)+"::"+items[1]+"--"+items[2]+"~"+str(i)+"::"+items[3]
-                    if inn_int in d:
-                        f.write(inn_int+"\t"+"\t".join(d[inn_int])+"\n")
-                    else:
-                        f.write(inn_int+"\t"+"\t".join([str(0) for x in range(length)])+"\n")
+            for element in l_inn:
+                items = element.split("_")
+                inn_int = items[0]+"~"+str(i)+"::"+items[1]+"--"+items[2]+"~"+str(i)+"::"+items[3]
+                if inn_int in d:
+                    all_inner.append(inn_int+"\t"+"\t".join(d[inn_int]))
+                else:
+                    all_inner.append(inn_int+"\t"+"\t".join([str(0) for x in range(length)]))
+        all_outer = []
         for i in range(48):#Number_of_molecules
-            with open(str(i)+"_all_outer.txt", "w") as f:
-                for elem in l_out:
-                    items = elem.split("_")
-                    out_int_d = items[0]+"~"+str(i)+"::"+items[1]+"--"+items[2]+"~"+"?"+"::"+items[3]
-                    out_int_ac = items[0]+"~"+"?"+"::"+items[1]+"--"+items[2]+"~"+str(i)+"::"+items[3]
-                    iis = re.split('~|::|--', out_int_d)
-                    iiss = re.split('~|::|--', out_int_ac)
-                    q = 0
-                    for key in d:
-                        if len(re.findall(iis[0]+"\~"+iis[1]+"::"+iis[2]+"--"+iis[3]+"\~"+"\d+"+"::"+iis[5],key)) != 0:
-                            f.write(key+"\t"+"\t".join(d[key])+"\n")
-                            q = 1
-                    if q == 0:
-                        f.write(out_int_d+"\t"+"\t".join([str(0) for x in range(length)])+"\n")
-                    p = 0
-                    for key in d:
-                        if len(re.findall(iiss[0]+"\~"+"\d+"+"::"+iiss[2]+"--"+iiss[3]+"\~"+iiss[4]+"::"+iiss[5],key)) != 0:
-                            f.write(key+"\t"+"\t".join(d[key])+"\n")
-                            p = 1
-                    if p == 0:
-                        f.write(out_int_ac+"\t"+"\t".join([str(0) for x in range(length)])+"\n")
+            for elem in l_out:
+                items = elem.split("_")
+                out_int_d = items[0]+"~"+str(i)+"::"+items[1]+"--"+items[2]+"~"+"?"+"::"+items[3]
+                out_int_ac = items[0]+"~"+"?"+"::"+items[1]+"--"+items[2]+"~"+str(i)+"::"+items[3]
+                iis = re.split('~|::|--', out_int_d)
+                iiss = re.split('~|::|--', out_int_ac)
+                q = 0
+                for key in d:
+                    if len(re.findall(iis[0]+"\~"+iis[1]+"::"+iis[2]+"--"+iis[3]+"\~"+"\d+"+"::"+iis[5],key)) != 0:
+                        all_outer.append(key+"\t"+"\t".join(d[key]))
+                        q = 1
+                if q == 0:
+                    all_outer.append(out_int_d+"\t"+"\t".join([str(0) for x in range(length)]))
+                p = 0
+                for key in d:
+                    if len(re.findall(iiss[0]+"\~"+"\d+"+"::"+iiss[2]+"--"+iiss[3]+"\~"+iiss[4]+"::"+iiss[5],key)) != 0:
+                        all_outer.append(key+"\t"+"\t".join(d[key]))
+                        p = 1
+                if p == 0:
+                    all_outer.append(out_int_ac+"\t"+"\t".join([str(0) for x in range(length)]))
+        return(all_inner, all_outer)
 
-def interesting_interactions():
+def interesting_interactions(l, all_inner, all_outer):
     import re
-    interesting_residues = ['GLU51', 'ASP52', 'ARG54', 'GLU24', 'LYS63'] #53!
-    for i in range(48):
-        with open(str(i)+"_all_inner.txt","r") as inn_f:
-            with open(str(i)+"_all_outer.txt", "r") as out_f:
-                with open(str(i)+"_interesting_interactions.txt", "w") as ii:
-                    inn_strings = inn_f.readlines()
-                    out_strings = out_f.readlines()
-                    for string in inn_strings:
-                        elems = string.split("\t")
-                        name = re.split('~|::|--', elems[0])
-                        if (name[0] in interesting_residues) or (name[3] in interesting_residues):
-                            ii.write(string)
-                    for string in out_strings:
-                        elems = string.split("\t")
-                        name = re.split('~|::|--', elems[0])
-                        if (name[0] in interesting_residues) or (name[3] in interesting_residues):
-                            ii.write(string)
+    interesting_interaction = []
+    for string in all_inner:
+        elems = string.split("\t")
+        name = re.split('~|::|--', elems[0])
+        if (name[0] in l) or (name[3] in l):
+            interesting_interaction.append(string)
+    for string in all_outer:
+        elems = string.split("\t")
+        name = re.split('~|::|--', elems[0])
+        if (name[0] in l) or (name[3] in l):
+            interesting_interaction.append(string)
+    return(interesting_interaction)
+
